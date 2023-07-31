9d23ad26cfcfa226833caecee9104bb921223cfa
fooyay <sean.coates+fooyay@gmail.com>
2023-07-31 09:33:27 -0400

    add storage

---
+++ PenguinStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract PenguinStorage {
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 7;
+    string favoriteNumberInText = "7";
+    address myAddress = 0xAD4ffd572677541ffc0B7CfE40BE6B4Db7Bc5cbB;
+    bytes32 favoriteBytes32 = "penguin";
+}
