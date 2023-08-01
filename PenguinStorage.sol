490570e6922815b2a9dda1d89ec3f4ff25a1bdbd
fooyay <sean.coates+fooyay@gmail.com>
2023-08-01 14:55:25 -0400

    make basic store, retrieve, addPerson

--- PenguinStorage.sol
+++ PenguinStorage.sol
@@ -2,9 +2,29 @@
 pragma solidity 0.8.18;
 
 contract PenguinStorage {
-    bool hasFavoriteNumber = true;
-    uint256 favoriteNumber = 7;
-    string favoriteNumberInText = "7";
-    address myAddress = 0xAD4ffd572677541ffc0B7CfE40BE6B4Db7Bc5cbB;
-    bytes32 favoriteBytes32 = "penguin";
+    uint256 myFavoriteNumber;
+
+    uint256[] listOfFavoriteNumbers;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
